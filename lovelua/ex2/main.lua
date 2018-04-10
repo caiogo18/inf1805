b3ece545ea214ac56f36f321b1f35acafa770783
caiogo18 <32112859+caiogo18@users.noreply.github.com>
2018-04-10 18:11:46 -0300

    exercicio2

---
+++ lovelua/ex2/main.lua
@@ -0,0 +1,89 @@
+function newblip (vel)
+  local x, y = 0, 0
+  local function wait(self)
+    self.isactiveafter=os.clock()+vel/15
+    coroutine.yield()
+  end
+  
+  return {
+    update = coroutine.wrap(function (self)
+      while(true) do
+        local width, height = love.graphics.getDimensions( )
+        x = x+15
+        if x > width then
+        -- volta para a esquerda da janela
+          x = 0
+        end
+        wait(self)
+      end
+    end),
+    affected = function (pos)
+      if pos>x and pos<x+10 then
+      -- "pegou" o blip
+        return true
+      else
+        return false
+      end
+    end,
+    draw = function ()
+      love.graphics.rectangle("line", x, y, 10, 10)
+    end,
+    isactiveafter=0
+  }
+end
+
+function newplayer ()
+  local x, y = 0, 200
+  local width, height = love.graphics.getDimensions( )
+  return {
+  try = function ()
+    return x
+  end,
+  update = function (dt)
+    x = x + 0.5
+    if x > width then
+      x = 0
+    end
+  end,
+  draw = function ()
+    love.graphics.rectangle("line", x, y, 30, 10)
+  end
+  }
+end
+
+function love.keypressed(key)
+  if key == 'a' then
+    pos = player.try()
+    for i in ipairs(listabls) do
+      local hit = listabls[i].affected(pos)
+      if hit then
+        table.remove(listabls, i) -- esse blip "morre" 
+        return -- assumo que apenas um blip morre
+      end
+    end
+  end
+end
+
+function love.load()
+  player =  newplayer()
+  listabls = {}
+  for i = 1, 5 do
+    listabls[i] = newblip(i)
+  end
+end
+
+function love.draw()
+  player.draw()
+  for i = 1,#listabls do
+    listabls[i].draw()
+  end
+end
+
+function love.update(dt)
+  player.update(dt)
+  for i = 1,#listabls do
+    if os.clock()>listabls[i].isactiveafter then
+      listabls[i]:update()
+    end
+  end
+end
