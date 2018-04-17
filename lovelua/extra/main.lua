016b80c23b3b0ca752b664eac912646fb3efa371
caiogo18 <32112859+caiogo18@users.noreply.github.com>
2018-04-17 19:03:47 -0300

    Add files via upload

--- lovelua/extra/main.lua
+++ lovelua/extra/main.lua
@@ -1,6 +1,6 @@
 
-function newblip (posx,posy,velm,velt,dir)
-  local x,y=posx,posy
+function newblip (posy,velm,velt,dir)
+  local x, y = 0, posy
   local function wait(self)
     self.isactiveafter=os.clock()+velm/100
     coroutine.yield()
@@ -9,10 +9,21 @@ function newblip (posx,posy,velm,velt,dir)
     update = coroutine.wrap(function (self)
         while(true) do
           local width, height = love.graphics.getDimensions( )
+          if dir==1 then
+            x = x+10
+            if x > width then
+                -- volta para a esquerda da janela
+              x = 0
+            end
+          else
+            x = x-10
+            if x < 0 then
+                -- volta para a esquerda da janela
+              x = width
+            end
+          end
           if math.random(20)==1 then
-            plx,ply=player.try()
-            shootx,shooty=normalize(plx-x,ply-y)
-            listashoots.enemy[#listashoots.enemy+1]=newshoot_enemy(x,y,velt,10*shootx,10*shooty)
+            listashoots.enemy[#listashoots.enemy+1]=newshoot_enemy(x,y,velt)
           end
           wait(self)
         end
@@ -32,7 +43,7 @@ function newblip (posx,posy,velm,velt,dir)
     isactiveafter=0
   }
 end
-function newshoot_enemy (posx,posy,vel,nx,ny)
+function newshoot_enemy (posx,posy,vel)
   local x, y = posx, posy
   local function wait(self)
     self.isactiveafter=os.clock()+vel/50
@@ -43,8 +54,7 @@ function newshoot_enemy (posx,posy,vel,nx,ny)
     update = coroutine.wrap(function (self)
       while(true) do
         local width, height = love.graphics.getDimensions( )
-        y = y + ny
-        x = x + nx
+        y = y+10
         xpl,ypl=player.try()
         if(x>=xpl and x<=xpl+20 and y>=ypl and y<=ypl+35) then
           life=life-1
@@ -88,6 +98,9 @@ function newshoot_friendly (posx,posy,vel)
         else
           for i in ipairs(listabls) do
             if listabls[i].affected(x,y) then
+              if(math.random(20)==1) then
+                listabuff[#listabuff+1]=new_item(x,y,'fast')
+              end
               table.remove(listabls,i)
               dead=true
               break
@@ -186,28 +199,55 @@ function newplayer (vel)
     end
     buffduration=os.clock()+30
   end,
-  isactiveafter=0,
-  
+  isactiveafter=0
   }
 end
 function new_wave()
-  local width, height = love.graphics.getDimensions( )
   level=level+1
-  auxx=width/5
-  auxy=200/(level+1)
+  aux=200/level
   for j = 1 , level do
-    for i = 1, 4 do
-      listabls[i+4*(j-1)] = newblip((i)*auxx,j*auxy,50,2.5,math.random(2))
+    for i = 1, 5 do
+      listabls[i+5*(j-1)] = newblip((j-1)*aux,math.random(10),math.random(10),math.random(2))
     end
   end
 end
 
+function new_item(posx,posy,buff)
+  local x, y = posx, posy
+  local function wait(self)
+    self.isactiveafter=os.clock()+0.01
+    coroutine.yield()
+  end
+
+  return {
+    update = coroutine.wrap(function (self)
+      while(true) do
+        local width, height = love.graphics.getDimensions( )
+        y = y+1
+        xpl,ypl=player.try()
+        if(x>=xpl and x<=xpl+20 and y>=ypl and y<=ypl+35) then
+          player.apply_buff(buff)
+          remove_shoot(self,listabuff)
+        elseif y > height then
+          remove_shoot(self,listabuff)
+        end
+        wait(self)
+      end
+    end),
+    draw = function()
+      --love.graphics.setColor(1, 1, 1)
+      love.graphics.polygon('fill',x,y+5,x+5,y,x+5,y+10,x+10,y+5)
+    end,
+    isactiveafter=0
+  }
+end
 function love.load()
   player =  newplayer(1)
   listabls = {}
   listashoots={}
   listashoots.friendly={}
   listashoots.enemy={}
+  listabuff={}
   life=3
   level=0
   new_wave()
@@ -224,15 +264,15 @@ function love.draw()
   for i = 1,#listashoots.friendly do
     listashoots.friendly[i].draw()
   end
+  for i = 1,#listabuff do
+    listabuff[i].draw()
+  end
   local width, height = love.graphics.getDimensions( )
   --love.graphics.setColor(1,1,1)
-  aux=width/9
-  for i=1,4 do
-    love.graphics.line(width/(4+1),3*height/4,width/4,3*height/4)
-  end
-  love.graphics.print("Level: " .. level, width-60, 0)
+  love.graphics.line(0,200,width,200)
+  love.graphics.print("Level: " .. level, width-60, height-40)
   if(life>=0) then
-    love.graphics.print("Life: " .. life, width-60, 20)
+    love.graphics.print("Life: " .. life, width-60, height-20)
   else
     love.graphics.print("Game Over!" , width/2, height/2,0,2,2)
     love.timer.sleep(1)
@@ -262,9 +302,17 @@ function love.update(dt)
       end
     end
   end
+  for i = 1,#listabuff do
+    if listabuff[i] then --bug
+      if os.clock()>listabuff[i].isactiveafter then
+        listabuff[i]:update()
+      end
+    end
+  end
   if #listabls == 0 then
     listashoots.friendly={}
     listashoots.enemy={}
+    listabuff={}
     new_wave()
   end
 end
@@ -276,7 +324,4 @@ function remove_shoot(shoot,lista)
     end
   end
 end
-function normalize(x,y)
-  local dist= math.sqrt(x*x+y*y)
-  return x/dist,y/dist
- end 
+
