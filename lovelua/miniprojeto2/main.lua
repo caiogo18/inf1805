fbe3dd8eb32aafa65126798c850c31309225419f
caiogo18 <32112859+caiogo18@users.noreply.github.com>
2018-04-19 12:44:14 -0300

    Add files via upload

--- lovelua/miniprojeto2/main.lua
+++ lovelua/miniprojeto2/main.lua
@@ -1,8 +1,8 @@
-
-function newblip (posx,posy,velm,velt,dir)
+LISPUP={'mov','shoot'}
+function newblip (posx,posy,velm,velt)
   local x,y=posx,posy
   local function wait(self)
-    self.isactiveafter=os.clock()+velm/100
+    self.isactiveafter=os.clock()+velm
     coroutine.yield()
   end
   return {
@@ -11,8 +11,8 @@ function newblip (posx,posy,velm,velt,dir)
           local width, height = love.graphics.getDimensions( )
           if math.random(20)==1 then
             plx,ply=player.try()
-            shootx,shooty=normalize(plx-x,ply-y)
-            listashoots.enemy[#listashoots.enemy+1]=newshoot_enemy(x,y,velt,10*shootx,10*shooty)
+            shootx,shooty=normalize(plx-(x+5),ply-(y+10))
+            listashoots.enemy[#listashoots.enemy+1]=newshoot_enemy(x+5,y+10,velt,10*shootx,10*shooty)
           end
           wait(self)
         end
@@ -29,13 +29,20 @@ function newblip (posx,posy,velm,velt,dir)
       --love.graphics.setColor(0,1,0,1)
       love.graphics.polygon("fill", x,y,x+10,y,x+5,y+10)
     end,
+    die = function (pos)
+      table.remove(listabls,pos)
+      if true then
+        listaPUP[#listaPUP+1]=new_powerup(x,y,LISPUP[1])
+        
+      end
+    end,
     isactiveafter=0
   }
 end
 function newshoot_enemy (posx,posy,vel,nx,ny)
   local x, y = posx, posy
   local function wait(self)
-    self.isactiveafter=os.clock()+vel/50
+    self.isactiveafter=os.clock()+vel
     coroutine.yield()
   end
 
@@ -73,7 +80,7 @@ end
 function newshoot_friendly (posx,posy,vel)
   local x, y = posx, posy
   local function wait(self)
-    self.isactiveafter=os.clock()+1/vel
+    self.isactiveafter=os.clock()+vel
     coroutine.yield()
   end
 
@@ -88,14 +95,7 @@ function newshoot_friendly (posx,posy,vel)
         else
           for i in ipairs(listabls) do
             if listabls[i].affected(x,y) then
-              table.remove(listabls,i)
-              dead=true
-              break
-            end
-          end
-          for i in ipairs(listabls) do
-            if listabls[i].affected(x,y) then
-              table.remove(listabls,i)
+              listabls[i].die(i)
               dead=true
               break
             end
@@ -129,10 +129,10 @@ function newplayer (vel)
   local width, height = love.graphics.getDimensions( )
   local x, y = (width/2)-15,2*height/3
   local shoot_delay=0
-  local movbuff=0
+  local movPup=0
   local buffduration=0
   local function wait(self)
-    self.isactiveafter=os.clock()+vel/(20+movbuff)
+    self.isactiveafter=os.clock()+vel/(1+movPup)
     coroutine.yield()
   end
   local function move(x1,y1)
@@ -168,8 +168,8 @@ function newplayer (vel)
         move(-10,0)
       end
       if love.keyboard.isDown('x') and shoot_delay<os.clock() then
-        listashoots.friendly[#listashoots.friendly+1]=newshoot_friendly(x+10,y,50)
-        shoot_delay=os.clock()+vel/6
+        listashoots.friendly[#listashoots.friendly+1]=newshoot_friendly(x+10,y,vel/2)
+        shoot_delay=os.clock()+10*vel
       end
       check_over()
       wait(self)
@@ -179,35 +179,76 @@ function newplayer (vel)
     --love.graphics.setColor(0,0,1)
     love.graphics.polygon('fill', x, y+20, x+10, y, x+20, y+20, x+10, y+30) 
   end,
-  apply_buff = function (buff)
+  apply_PUP = function (pup)
     reset_buff()
-    if buff=='fast' then
-      movbuff=20
+    if pup=='mov' then
+      movPup=1
     end
     buffduration=os.clock()+30
   end,
-  isactiveafter=0,
+  isactiveafter=0
   
+  }
+end
+function new_powerup(posx,posy,pup)
+  local x, y = posx, posy
+  local function wait(self)
+    self.isactiveafter=os.clock()+0.01
+    coroutine.yield()
+  end
+
+  return {
+    update = coroutine.wrap(function (self)
+      while(true) do
+        local width, height = love.graphics.getDimensions( )
+        y = y+1
+        xpl,ypl=player.try()
+        if(x>=xpl and x<=xpl+20 and y>=ypl and y<=ypl+35) then
+          print(pup)
+          player.apply_PUP(pup)
+          remove_shoot(self,listaPUP)
+        elseif y > height then
+          remove_shoot(self,listaPUP)
+        end
+        wait(self)
+      end
+    end),
+    draw = function()
+      --love.graphics.setColor(1, 1, 1)
+      love.graphics.polygon('fill',x,y+5,x+5,y,x+5,y+10,x+10,y+5)
+    end,
+    isactiveafter=0
   }
 end
 function new_wave()
   local width, height = love.graphics.getDimensions( )
   level=level+1
-  auxx=width/5
-  auxy=200/(level+1)
-  for j = 1 , level do
-    for i = 1, 4 do
-      listabls[i+4*(j-1)] = newblip((i)*auxx,j*auxy,50,2.5,math.random(2))
+  auxx=width/9
+  enemies=2*level+2
+  auxy=200/((enemies/5)+1)
+  local j=0
+  while enemies>0 do
+    if enemies>5 then
+      for  i = 1, 5 do
+        listabls[i+5*j] = newblip(2*(i-1)*auxx+auxx/2,(j+1)*auxy,0.5,0.04)
+      end
+    else
+      for  i = 1, enemies do
+        listabls[i+5*j] = newblip(2*(i-1)*auxx+auxx/2,(j+1)*auxy,0.5,0.04)
+      end
     end
+    enemies=enemies-5
+    j=j+1
   end
 end
 
 function love.load()
-  player =  newplayer(1)
+  player =  newplayer(0.04)
   listabls = {}
   listashoots={}
   listashoots.friendly={}
   listashoots.enemy={}
+  listaPUP={}
   life=3
   level=0
   new_wave()
@@ -224,11 +265,14 @@ function love.draw()
   for i = 1,#listashoots.friendly do
     listashoots.friendly[i].draw()
   end
+  for i = 1,#listaPUP do
+    listaPUP[i].draw()
+  end
   local width, height = love.graphics.getDimensions( )
   --love.graphics.setColor(1,1,1)
   aux=width/9
   for i=1,4 do
-    love.graphics.line(width/(4+1),3*height/4,width/4,3*height/4)
+    love.graphics.line(aux*(2*(i-1)+1),3*height/4,aux*(2*i),3*height/4)
   end
   love.graphics.print("Level: " .. level, width-60, 0)
   if(life>=0) then
@@ -240,25 +284,33 @@ function love.draw()
 end
 
 function love.update(dt)
-  if os.clock()>player.isactiveafter then
+  time=os.clock()
+  if time>player.isactiveafter then
     player:update()
   end
   for i = 1,#listabls do
-    if os.clock()>listabls[i].isactiveafter then
+    if time>listabls[i].isactiveafter then
       listabls[i]:update()
     end
   end
   for i = 1,#listashoots.enemy do
     if listashoots.enemy[i] then --bug
-      if os.clock()>listashoots.enemy[i].isactiveafter then
+      if time>listashoots.enemy[i].isactiveafter then
         listashoots.enemy[i]:update()
       end
     end
   end
   for i = 1,#listashoots.friendly do
     if listashoots.friendly[i] then --bug
-      if os.clock()>listashoots.friendly[i].isactiveafter then
+      if time>listashoots.friendly[i].isactiveafter then
         listashoots.friendly[i]:update()
+      end
+    end
+  end
+  for i = 1,#listaPUP do
+    if listaPUP[i] then --bug
+      if time>listaPUP[i].isactiveafter then
+        listaPUP[i]:update()
       end
     end
   end
